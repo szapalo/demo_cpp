f2118afa4aa03ef4758cee0f95c6ae32af08a1eb
Simon Zapalowicz <s.zapalowicz@gmail.com>
2023-11-10 17:58:46 -0800

    Update List.h and Tensor.h

--- List.h
+++ List.h
@@ -1,19 +1,26 @@
 
 #ifndef NUNU_LIST_H_
 #define NUNU_LIST_H_
-#include "./Pair.h"
 
-#include <cstdlib>
+
 #include <iostream>
-
+#include <cstring> //memcpy
+#include <stdexcept>
 
 template <typename T>
-class List{
+class List {
     private:
 
     T* data = nullptr;
-    int full_size = 0; // size of allocated space (2^n)
-    int size = 0; // size of array in allocated space (size <= full_size)
+    unsigned int full_size = 0; // size of allocated space (2^n)
+    unsigned int size = 0; // size of array in allocated space (size <= full_size)
+
+    template <typename C>
+    void _assign(const C& lst){
+        for(int i=0; i<size; ++i){
+            *(data+i) = lst[i];
+        }
+    }
 
     void _increment(){
         if(size==full_size){
@@ -30,76 +37,116 @@ class List{
     }
 
     void _decrement(){
+        if(!size){return;}
         if(size==(full_size>>1)+1){
             full_size>>=1;
-            _resize_list();
         }
-        --size;
+        if(full_size){
+            _resize_list();
+            --size;
+        }
+        else{
+            clear();
+        }
     }
 
     void _allocate_list(){
-        data = (T*) malloc(sizeof(T)*full_size);
+        data = (T*) ::operator new(sizeof(T)*full_size);
     }
 
     void _resize_list(){
-        data = (T*) realloc(data, sizeof(T)*full_size);
+        // should be called only if full_size>0; and old full_size != new full_size;
+        T* new_data = (T*) ::operator new(sizeof(T)*full_size);
+
+        if(size < full_size){
+            std::copy(data, data+size, new_data);
+            // std::memcpy(new_data, data, sizeof(T)*size);
+        }
+        else{
+            std::copy(data, data+full_size, new_data);
+            // std::memcpy(new_data, data, sizeof(T)*full_size);
+        }
+        ::operator delete(data);
+        data = new_data;
+    }
+
+    bool _set_full_size(unsigned int length){
+        unsigned int tmp_val =0;
+        // Set full_size = 2^n s.t. n>=full_size
+        if(length){
+            tmp_val=1;
+            while (tmp_val<length){
+                tmp_val<<=1;
+            }
+        }
+
+        if(tmp_val!=full_size){
+            full_size = tmp_val;        
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public:
 
     List(){}
 
-    List(unsigned int length):size(length){
-        full_size=1;
-        while (full_size<length){
-            full_size<<=1;
+    List(int length):size(length){
+        if(length < 0){
+            throw std::underflow_error("Negative input size.");
         }
-        _allocate_list();
+        if(_set_full_size(length)){
+            _allocate_list();
+        }
     }
 
-    List(const T& val, int length):List(length){
+    List(int length, const T& val):List(length){
         for (int i = 0; i < size; i++)
         {
             *(data+i) = val;
         }
     }
 
-    // template <typename C>
-    List(const std::initializer_list<T>& lst):List(lst.size()){
-        auto xx = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
+    List(const std::initializer_list<T>& lst):List(lst.size()){  
+        auto it = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
         for(int i=0; i<size; ++i){
-            *(data+i) = *(xx+i);
+            *(data+i) = *(it+i);
         }
     }
-
-    // Copy Constructor
+    // Copy Constructor of List<T> 
+    // Note: For some reason, the templated version does not recognize List<T>.
+    //       So we must define one specifically.
+    //       Perhaps it wont be an issue once we derive from an abstract class.
+    List(const List<T>& lst) : List(lst.length()){
+        _assign(lst);
+    }
+    // Copy Constructor from miscelaneous List
     template <typename C>
-    List(const List<C>& lst): List(lst.length()){
-        for(int i=0; i<size; ++i){
-            *(data+i) = lst[i];
-        }
+    List(const List<C>& lst) : List(lst.length()){
+        _assign(lst);
     }
-
 
     ~List(){
-        free(data);
-
+        ::operator delete(data);
     }
 
     void clear(){
         size=0;
-        free(data);
+        ::operator delete(data);
         data = nullptr;
         full_size = 0;
     }
 
     friend std::ostream& operator<<(std::ostream& strm, const List &lst ){
         strm << "[";
-        for(int i =0; i<lst.size-1; ++i){
-            strm << lst[i] <<", ";
-        }
-        if(lst.size>0){
-            strm << lst[lst.size-1];
+        unsigned int n = lst.length();
+        if(n){
+            for(int i=0; i<n-1; ++i){            
+                strm << lst[i] <<", ";
+            }
+            strm << lst[n-1];
         }
         strm << "]";
         return strm;
@@ -111,26 +158,27 @@ class List{
             + std::to_string(size) + ")";
     }
 
-    bool isempty(){
-        return !size;
-    }
-
-    bool empty(){
-        return !size;
-    }
-
+    bool empty() const {return !size;}
 
     void append(const T& val){
         _increment();
         *(data+size-1) = val;
     }
-
     void push_back(const T& val){
         append(val);
     }
-
     void push(const T& val){
         append(val);
+    }
+
+    template <typename C>
+    void extend(const List<C>& lst){
+        int prev_size = size;
+        resize(size+lst.length());
+
+        for(int i=0; i<lst.length(); ++i){
+            *(data+prev_size+i) = lst[i];
+        }
     }
 
     void remove_elem(T val){
@@ -144,6 +192,9 @@ class List{
     }
 
     void remove(int rem_idx){
+        if(rem_idx <= 0){
+            throw std::underflow_error("Negative input size.");
+        }
         T* last_elem = end();
         for (T el = data+rem_idx+1; el < last_elem; el++)
         {
@@ -154,12 +205,10 @@ class List{
 
     List<T>& operator=(const List<T>& lst) {
         resize(lst.length());
-        for(int i=0; i<size; ++i){
-            *(data+i) = lst[i];
-        }
+        _assign(lst);
         return *this;
     }
-
+    
     T& operator[](const unsigned int& idx) const {
         return *(data + idx);
     }
@@ -168,7 +217,7 @@ class List{
         return data+idx;
     }
 
-    bool operator==(List<T> lst) const{
+    bool operator==(const List<T>& lst) const{
         if(lst.length() != size){
             return false;
         }
@@ -179,10 +228,25 @@ class List{
         }
         return true;
     }
-    
-    void resize(int x){
-        full_size = size = x;
-        _resize_list();
+    bool operator!=(const List<T>& lst){
+        return !(*this == lst);
+    }
+
+    void resize(int length){
+        if(length < 0){
+            throw std::underflow_error("Negative input length.");
+        }
+        // TODO: resize where full_size = 2^n s.t. n>=0
+        if(_set_full_size(length)){
+            if(!size){
+                _allocate_list();
+            }
+            else{
+                _resize_list();
+            }
+        }
+        size = length;
+
     }
 
     T* begin() const {
@@ -201,18 +265,17 @@ class List{
         return *(data+size-1);
     }
 
-    int length() const {
+    unsigned int length() const {
         return size;
     }
 
     // should be static?
     void print() const {
         std::cout<<"[";
-        
-        for(int i=0; i<size-1; ++i){
-            std::cout<<data[i]<<",";
-        }
-        if(size>0){
+        if(size){
+            for(int i=0; i<size-1; ++i){
+                std::cout<<data[i]<<",";
+            }
             std::cout<<data[size-1];
         }
         std::cout<<"]"<<std::endl;
--- Tensor.h
+++ Tensor.h
@@ -4,25 +4,46 @@
 
 
 #include "./NestedInitializerList.h"
-#include <cstdarg>
-#include <iostream>
-#include <time.h>
 #include "./List.h"
 
-template<int N, typename T = float>
-class Tensor{
+#include <cstdarg>
+#include <iostream>
+#include <stdexcept>
+	
+template<int N, typename T = float> requires (N>0 && N < 9)
+class Tensor {
 
     private:
 
     unsigned int* _dims = nullptr;
-    Tensor<N-1, T>* data = nullptr;
+    Tensor<N-1, T>* _data = nullptr;
     friend Tensor<N+1, T>;
 
-    void _malloc_data(){
-        data = (Tensor<N-1, T>*) malloc(sizeof(Tensor<N-1, T>)*(*_dims));
+    static void assert_dims(const List<int>& dims){
+        int n = dims.length();
+        if(n != N){
+            throw std::range_error("Length of dims List does not match N.");
+        }
+        for(int i=0; i<n; ++i){
+            if(dims[i] <= 0){
+                throw std::underflow_error(
+                    "Non-positive element of dims : dims[i] <= 0");
+            }
+        }
+    }
 
+    void _malloc_dims(const List<int>& dims){
+        Tensor<N,T>::assert_dims(dims);
+        _dims = new unsigned int[N];
+        for(int i=0; i<dims.length(); ++i){
+            *(_dims+i) = dims[i];
+        }
+    }
+
+    void _malloc_data(){
+        _data = (Tensor<N-1, T>*) ::operator new(sizeof(Tensor<N-1, T>)*(*_dims));
         for(int i=0; i<*_dims; ++i){
-            new (data + i) Tensor<N-1, T>(_dims+1);
+            new (_data + i) Tensor<N-1, T>(_dims+1);
         }
     }
 
@@ -30,61 +51,64 @@ class Tensor{
         for(auto& elem: *this){
             elem._clear(false);
         }
-        free(data);
         if(clear_dims){
-            free(_dims);
+            delete[] _dims;
+        }
+        ::operator delete(_data);
+
+    }
+
+    void _assign(const Tensor<N>& tensor){
+        for(int i=0; i<length(); ++i){
+            (_data + i)->_assign(tensor[i]);
         }
     }
 
-    Tensor(unsigned int* dims ):_dims(dims){
+    Tensor(unsigned int* dims):_dims(dims){
         _malloc_data();
     }
 
     public:
 
-    Tensor(unsigned int arg,...){
-        _dims = (unsigned int*) malloc(sizeof(unsigned int)*N);
-        *_dims = arg;
+    Tensor(int arg,...){
+        List<int> dims_lst(N, arg);
         va_list args;
         va_start(args, arg);
-        for (int i = 1; i < N; ++i) {
-            *(_dims+i) = va_arg(args, unsigned int);
+        for (int i = 1; i < N ; ++i) {
+            dims_lst[i] = va_arg(args, int);
         }
         va_end(args);
+
+        _malloc_dims(dims_lst);
         _malloc_data();
     }
 
 
-    Tensor(const List<unsigned int>& lst){
-        _dims = (unsigned int*) malloc(sizeof(unsigned int)*N);
-        for(int i=0; i<N; ++i){
-            *(_dims+i) = lst[i];
-        }
-        int size = *_dims;
-        data = (Tensor<N-1, T>*) malloc(sizeof(Tensor<N-1, T>)*size);
-        for(int i=0; i<size; ++i){
-            new (data + i) Tensor<N-1, T>(_dims+1);
-        }
+    Tensor(const List<int>& lst){
+        _malloc_dims(lst);
+        _malloc_data();
     }
 
     /* Deep Copy */
-    Tensor(const Tensor<N, T>& tensor){
+    Tensor(const Tensor<N,T>& tensor):Tensor(tensor.dims()){
         int size = tensor.length();
-        data = (Tensor<N-1, T>*) malloc(sizeof(Tensor<N-1, T>)*size);
         for(int i=0; i<size; ++i){
-            new (data + i) Tensor<N-1, T>(*tensor(i));
+            *(_data + i) = tensor[i];
         }
     }
 
     Tensor(const NestedInitializerLists<N,T>& lst, unsigned int* d=nullptr){
         int size = lst.size();
-        _dims = d? d : (unsigned int*) malloc(sizeof(unsigned int)*N);
+        if(size == 0){
+            throw std::underflow_error("NestedList is empty.");
+        }
+        _dims = d? d : new unsigned int[N];
         *_dims = size;
-        data = (Tensor<N-1, T>*) malloc(sizeof(Tensor<N-1, T>)*size);
-        auto xx = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
+        _data = (Tensor<N-1, T>*) ::operator new(sizeof(Tensor<N-1, T>)*size);
+        auto iter = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
 
         for(int i=0; i<size; ++i){
-            new (data + i) Tensor<N-1, T>(*(xx+i), _dims+1);
+            new (_data + i) Tensor<N-1, T>(*(iter+i), _dims+1);
         }
     }
 
@@ -93,7 +117,11 @@ class Tensor{
         _clear(true);
     }
 
-    List<int> dims(){
+    unsigned int dim(unsigned int i) const {
+        return *(_dims+i);
+    } 
+
+    List<int> dims() const {
         List<int> result(N);
         for(int i=0; i<N; ++i){
             result[i] = *(_dims+i);
@@ -101,50 +129,81 @@ class Tensor{
         return result;
     }
 
-    void print(){
-        for(auto& elem : *this){
-            elem.print();
+    void print(char start=' ', char end=' ') const {
+        int n = length()-1;
+        std::cout<<start<<"{ \n";
+        for(int i=0; i<n; ++i){
+            (_data+i)->print('\t', ',');
         }
-        std::cout<<std::endl;
+        (_data+n)->print('\t', ' ');
+        std::cout<<" }"<<end<<"\n";
     }
 
     /* Auto range for loop */
-    Tensor<N-1, T>* begin(){
-        return data;
+    Tensor<N-1, T>* begin() const {
+        return _data;
     }
 
-    Tensor<N-1, T>* end(){
-        return data + *_dims;
+    Tensor<N-1, T>* end() const {
+        return _data + *_dims;
     }
 
     /* Index operators */
     Tensor<N-1, T>& operator[](const unsigned int &idx) const {
-        return *(data + idx);
+        return *(_data + idx);
     }
 
     Tensor<N-1, T>* operator()(const unsigned int &idx) const {
-        return data + idx;
+        return _data + idx;
     }
 
     /* Assign */
-
+    // TODO: assign operator must return Type&
     template<typename C>
-    void operator=(const C& val){
+    Tensor<N,T>& operator=(const C& val){
         for(int i=0; i<length(); ++i){
-            *(data + i) = val;
+            *(_data + i) = val;
         }
+        return *this;
     }
 
-    void operator=(Tensor<N, T>& mat){
-        for(int i=0; i<length(); ++i){
-            *(data + i) = mat[i];
-        }
+
+    Tensor<N,T>& operator=(const Tensor<N,T>& mat){
+        _assign(mat);
+        return *this;
     }
 
-    int length(){
+    unsigned int length() const {
         return *_dims;
     }
 
+    void resize(int new_size){
+        if(new_size <= 0){
+            throw std::underflow_error("Non-positive resize input.");
+        }
+        int curr_size = length();
+        
+        if(curr_size == new_size){ return; }
+        if(curr_size > new_size){
+            // delete excess Tensor<N-1,T>[new_size :: dims-1]
+            for(int i=new_size; i<curr_size; ++i){
+                (_data+i)->_clear(false);
+            }
+        }
+        
+        Tensor<N-1,T>* _new_ptr = (Tensor<N-1,T>*) ::operator new(
+                sizeof(Tensor<N-1, T>)*(new_size)
+            );        
+        
+        _data = _new_ptr;
+        *_dims = new_size;
+        
+        if(curr_size < new_size ){
+            for(int i=curr_size; i<new_size; ++i){
+                new (_data + i) Tensor<N-1, T>(_dims+1);
+            }
+        }
+    }
 
     /* Addition */
     template<typename C>
@@ -154,21 +213,21 @@ class Tensor{
         }
     }
 
-    void operator+=(const Tensor<N, T>& mat){
+    void operator+=(const Tensor<N,T>& mat){
         for(int i=0; i<length(); ++i){
-            *(data + i) += mat[i];
+            *(_data + i) += mat[i];
         }
     }
 
     template<typename C>
-    const Tensor<N, T> operator+(const C& val) const{
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator+(const C& val) const {
+        Tensor<N,T> new_tensor = *this;
         new_tensor += val;
         return new_tensor;
     }
 
-    const Tensor<N, T> operator+(const Tensor<N, T>& tensor){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator+(const Tensor<N,T>& tensor){
+        Tensor<N,T> new_tensor = *this;
         new_tensor += tensor;
         return new_tensor;
     }
@@ -182,21 +241,21 @@ class Tensor{
         }
     }
 
-    void operator-=(const Tensor<N, T>& mat){
+    void operator-=(const Tensor<N,T>& mat){
         for(int i=0; i<length(); ++i){
-            *(data + i) -= mat[i];
+            *(_data + i) -= mat[i];
         }
     }
 
     template<typename C>
-    const Tensor<N, T> operator-(const C& val) const{
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator-(const C& val) const{
+        Tensor<N,T> new_tensor = *this;
         new_tensor -= val;
         return new_tensor;
     }
 
-    const Tensor<N, T> operator-(const Tensor<N, T>& tensor){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator-(const Tensor<N,T>& tensor){
+        Tensor<N,T> new_tensor = *this;
         new_tensor -= tensor;
         return new_tensor;
     }
@@ -211,21 +270,21 @@ class Tensor{
         }
     }
 
-    void operator*=(const Tensor<N, T>& tensor){
+    void operator*=(const Tensor<N,T>& tensor){
         for(int i=0; i<length(); ++i){
-            *(data + i) *= tensor[i];
+            *(_data + i) *= tensor[i];
         }
     }
 
     template<typename C>
-    const Tensor<N, T>& operator*(const C& val){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T>& operator*(const C& val){
+        Tensor<N,T> new_tensor = *this;
         new_tensor *= val;
         return new_tensor;
     }
 
-    const Tensor<N, T>& operator*(const Tensor<N, T>& tensor){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T>& operator*(const Tensor<N,T>& tensor){
+        Tensor<N,T> new_tensor = *this;
         new_tensor *= tensor;
         return new_tensor;
     }
@@ -238,21 +297,21 @@ class Tensor{
         }
     }
 
-    void operator/=(const Tensor<N, T>& tensor){
+    void operator/=(const Tensor<N,T>& tensor){
         for(int i=0; i<length(); ++i){
-            *(data + i) /= tensor[i];
+            *(_data + i) /= tensor[i];
         }
     }
 
     template<typename C>
-    const Tensor<N, T> operator/(const C& val){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator/(const C& val){
+        Tensor<N,T> new_tensor = *this;
         new_tensor /= val;
         return new_tensor;
     }
 
-    const Tensor<N, T> operator/(const Tensor<N, T>& tensor){
-        Tensor<N, T> new_tensor = *this;
+    const Tensor<N,T> operator/(const Tensor<N,T>& tensor){
+        Tensor<N,T> new_tensor = *this;
         new_tensor /= tensor;
         return new_tensor;
     }
@@ -262,58 +321,74 @@ class Tensor{
 
 
 template <typename T>
-class Tensor<1, T>{
+class Tensor<1,T> {
     private:
     friend Tensor<2, T>;
 
     unsigned int* _dims = nullptr;
-    T* data = nullptr;
+    T* _data = nullptr;
 
     void _clear(const bool clear_dims){
-        free(data);
         if(clear_dims){
-            free(_dims);
+            delete[] _dims;
+        }
+        ::operator delete(_data);
+
+    }
+
+    void _malloc_dims(int dim){
+        if( dim <= 0){
+            throw std::underflow_error("Non-positive dim input value.");
+        }
+        _dims = new unsigned int[1];
+        *(_dims) = dim;
+    }
+
+    void _malloc_data(){
+        _data = (T*) ::operator new(sizeof(T)*(*_dims));
+    }
+
+    void _assign(const Tensor<1> tensor){
+        for(int i=0; i<length(); ++i){
+            *(_data + i) = tensor[i];
         }
     }
 
     Tensor(unsigned int* dims):_dims(dims){
-        data = (T*) malloc(sizeof(T)*(*_dims));
+        _malloc_data();
     }
 
     public:
-    int length(){
+    unsigned int length() const {
         return *_dims;
     }
 
-    Tensor(unsigned int dim){
-        _dims = (unsigned int*) malloc(sizeof(unsigned int));
-        *_dims = dim;
-        data = (T*) malloc(sizeof(T)*dim);
+    Tensor(int dim){
+        _malloc_dims(dim);
+        _malloc_data();
     }
 
     /* Deep Copy */
-    Tensor( const Tensor<1, T>& tensor){
+    Tensor( const Tensor<1,T>& tensor):Tensor(tensor.dims()){
         int size = tensor.length();
-        data = (T*) malloc(sizeof(T)*size);
         for(int i=0; i<size; ++i){
-            *(data + i) = tensor[i];
+            *(_data + i) = tensor[i];
         }
     }
 
-    Tensor(const List<unsigned int>& lst){
-        _dims = (unsigned int*) malloc(sizeof(unsigned int));
-        *_dims = lst[0];
-        data = (T*) malloc(sizeof(T)*(*_dims));
-    }
+    Tensor(const List<int>& lst):Tensor(lst[0]){}
 
     Tensor(const NestedInitializerLists<1,T>& lst, unsigned int* d=nullptr){
         int size = lst.size();
-        _dims = d? d : (unsigned int*) malloc(sizeof(unsigned int));
+        if(size == 0){
+            throw std::underflow_error("NestedList is empty.");
+        }
+        _dims = d? d : new unsigned int[1];
         *_dims = size;
-        data = (T*) malloc(sizeof(T)*size);
-        auto xx = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
+        _malloc_data();
+        auto iter = lst.begin(); //why doesnt it work when I do std::initializer_list<int>::iterator ?
         for(int i=0; i<size; ++i){
-            *(data + i) = *(xx+i);
+            *(_data + i) = *(iter+i);
         }
     }
 
@@ -321,55 +396,74 @@ class Tensor<1, T>{
         _clear(true);
     }
 
-    List<int> dims(){
+    unsigned int dim(unsigned int i){
+        return *(_dims);
+    } 
+
+    List<int> dims() const {
             List<int> result(1);
             result[0] = *_dims;
         return result;
     }
 
-    friend std::ostream& operator<<(std::ostream &strm, const Tensor<1, T> &a) {
+    void resize(int new_size){ // TO FIX!!!
+        if(new_size <= 0){
+            throw std::underflow_error("Non-positive resize input.");
+        }
+        if(length() == new_size){ return; }
+        _clear(false);        
+        *_dims = new_size;
+        _data = (T*) ::operator new(sizeof(T)*(new_size));
+
+    }
+
+
+    friend std::ostream& operator<<(std::ostream &strm, const Tensor<1,T> &a) {
         return strm << "A(" << a.length() << ")";
     }
 
-    void print(){
-        for(auto& elem : *this){
-            std::cout<<elem<<" ";
+    void print(char start=' ', char end=' ') const {
+        std::cout<<start<<"{ ";
+        int n=length()-1;
+        for(int i=0; i<length()-1; ++i){
+            std::cout<<*(_data+i)<<", ";
         }
-        std::cout<<std::endl;
+        std::cout<<*(_data+n)<<" }"<<end<<std::endl;
     }
 
     /* Auto range for loop */
 
-    T* begin(){
-        return data;
+    T* begin() const {
+        return _data;
     }
 
-    T* end(){
-        return data + *_dims;
+    T* end() const {
+        return _data + *_dims;
     }
 
     /* Index operators */
 
     T& operator[](const unsigned int &idx) const {
-        return *(data + idx);
+        return *(_data + idx);
     }
 
     T* operator()(const unsigned int &idx) const {
-        return data + idx;
+        return _data + idx;
     }
 
     /* Assign */
-
-    void operator=(const T& val){
+    Tensor<1,T>& operator=(const T& val){
         for(int i=0; i<length(); ++i){
-            *(data + i) = val;
+            *(_data + i) = val;
         }
+        return *this;
     }
 
-    void operator=(const Tensor<1, T>& lst){
+    Tensor<1,T>& operator=(const Tensor<1,T>& lst){
         for(int i=0; i<length(); ++i){
-            *(data + i) = lst[i];
+            *(_data + i) = lst[i];
         }
+        return *this;
     }
 
     /* Addition */
@@ -380,22 +474,22 @@ class Tensor<1, T>{
         }
     }
 
-    void operator+=(const Tensor<1, T>& lst){
+    void operator+=(const Tensor<1,T>& lst){
         for(int i=0; i<length(); ++i){
-            data[i]+=lst[i];
+            _data[i]+=lst[i];
         }
     }
 
-    Tensor<1, T> operator+(const T& val){
-        Tensor<1, T> new_tensor = *this;
+    Tensor<1,T> operator+(const T& val){
+        Tensor<1,T> new_tensor = *this;
         new_tensor += val;
         return new_tensor;
     }
 
-    Tensor<1, T> operator+(const Tensor<1, T>& tensor){
-        Tensor<1, T>* new_tensor = new Tensor<1, T>(*this);
-        (*new_tensor) += tensor;
-        return *new_tensor;
+    Tensor<1,T> operator+(const Tensor<1,T>& tensor){
+        Tensor<1,T> new_tensor = *this;
+        new_tensor += tensor;
+        return new_tensor;
     }
 
     /* Subtraction */
@@ -406,20 +500,20 @@ class Tensor<1, T>{
         }
     }
 
-    void operator-=(const Tensor<1, T>& lst){
+    void operator-=(const Tensor<1,T>& lst){
         for(int i=0; i<length(); ++i){
-            data[i] -= lst[i];
+            _data[i] -= lst[i];
         }
     }
 
-    Tensor<1, T> operator-(const T& val){
-        Tensor<1, T> new_tensor = *this;
+    Tensor<1,T> operator-(const T& val){
+        Tensor<1,T> new_tensor = *this;
         new_tensor -= val;
         return new_tensor;
     }
 
-    Tensor<1, T> operator-(const Tensor<1, T>& tensor){
-        Tensor<1, T>* new_tensor = new Tensor<1, T>(*this);
+    Tensor<1,T> operator-(const Tensor<1,T>& tensor){
+        Tensor<1,T>* new_tensor = new Tensor<1,T>(*this);
         (*new_tensor) -= tensor;
         return *new_tensor;
     }
@@ -432,22 +526,22 @@ class Tensor<1, T>{
         }
     }
 
-    void operator*=(const Tensor<1, T> lst){
+    void operator*=(const Tensor<1,T> lst){
         for(int i=0; i<length(); ++i){
-            data[i] *= lst[i];
+            _data[i] *= lst[i];
         }
     }
 
-    Tensor<1, T> operator*(const T& val){
-        Tensor<1, T> new_tensor = *this;
-        (*new_tensor) *= val;
-        return *new_tensor;
+    Tensor<1,T> operator*(const T& val){
+        Tensor<1,T> new_tensor = *this;
+        new_tensor *= val;
+        return new_tensor;
     }
 
-    Tensor<1, T> operator*(const Tensor<1, T>& tensor){
-        Tensor<1, T> new_tensor = *this;
-        (*new_tensor) *= tensor;
-        return *new_tensor;
+    Tensor<1,T> operator*(const Tensor<1,T>& tensor){
+        Tensor<1,T> new_tensor = *this;
+        new_tensor *= tensor;
+        return new_tensor;
     }
 
     /* Division */
@@ -458,21 +552,21 @@ class Tensor<1, T>{
         }
     }
 
-    void operator/=(const Tensor<1, T> lst){
+    void operator/=(const Tensor<1,T> lst){
         for(int i=0; i<length(); ++i){
-            data[i] /= lst[i];
+            _data[i] /= lst[i];
         }
     }
 
-    Tensor<1, T> operator/(const T& val){
-        Tensor<1, T> new_tensor = *this;
-        (*new_tensor) /= val;
+    Tensor<1,T> operator/(const T& val){
+        Tensor<1,T> new_tensor = *this;
+        new_tensor /= val;
         return *new_tensor;
     }
 
-    Tensor<1, T> operator/(const Tensor<1, T>& tensor){
-        Tensor<1, T> new_tensor = *this;
-        (*new_tensor) /= tensor;
+    Tensor<1,T> operator/(const Tensor<1,T>& tensor){
+        Tensor<1,T> new_tensor = *this;
+        new_tensor /= tensor;
         return *new_tensor;
     }
 
